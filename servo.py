c082af2236f635d0f1dd5a30ad418c675fdc27ff
torjeikenes <torje.n.eikenes@gmail.com>
2016-06-27 20:17:27 +0000

    servo

--- servo.py
+++ servo.py
@@ -1,4 +1,4 @@
-import RPI.GPIP as GPIO
+import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
@@ -10,11 +10,11 @@ p.start(7.5)
 
 try:
         while True:
-                p.ChangeDutyCycle(7.5)
+                p.ChangeDutyCycle(5)
                 time.sleep(1)
+                #p.ChangeDutyCycle(12.5)
+                #time.sleep(1)
                 p.ChangeDutyCycle(12.5)
-                time.sleep(1)
-                p.ChangeDutyCycle(2.5)
                 time.sleep(1)
 
 except KeyboardInterrupt:
