f67ea70d1be8070ee3916120dbda0ea0538d2a6c
torjeikenes <torje.n.eikenes@gmail.com>
2016-06-28 21:32:10 +0000

    Servo works when ring is typed

--- slack_bot.py
+++ slack_bot.py
@@ -8,26 +8,34 @@ BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 # What channel your bot uses
 CHANNEL_NAME = "general"
 
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(7,GPIO.OUT)
-
-p = GPIO.PWM(7,50)
-p.start(7.5)
-
 def servo():
-        p.ChangeDutyCycle(7.5)
-        time.sleep(1)
-        p.ChangeDutyCycle(2.5)
-        time.sleep(1)
+	GPIO.setmode(GPIO.BOARD)
 
+	GPIO.setup(7,GPIO.OUT)
+
+	p = GPIO.PWM(7,50)
+	p.start(7.5)
+
+	p.ChangeDutyCycle(10)
+        time.sleep(0.25)
+
+        p.ChangeDutyCycle(3)
+        time.sleep(1)
+	
+        p.ChangeDutyCycle(6)
+        time.sleep(0.5)
+	
+	p.stop()
+
+	GPIO.cleanup()
 def main():
+    print "main"
     # Creates a slackclient instance with bots token
     sc = SlackClient(BOT_TOKEN)
     
     #Connect to slack
     if sc.rtm_connect():
-        
+        print "connect" 
         while True:
             # Read latest messages
             for slack_message in sc.rtm_read():
@@ -38,14 +46,10 @@ def main():
                 # If someone types ring, the bot will post a bell
                 if(message == "ring"):
                     sc.rtm_send_message(CHANNEL_NAME, ":bell:")
-                    
+                    servo() 
             # Sleeps for one second
             time.sleep(1)
 
-try:
-    if __name__ == '__main__':
-        main()
-except KeyboardInterrupt:
-    p.stop()
 
-    GPIO.cleanup()
+if __name__ == '__main__':
+    main()
