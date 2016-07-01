24ad13948c72b645409c065462521b3ef42ff782
torjeikenes <torje.n.eikenes@gmail.com>
2016-07-01 20:44:42 +0000

    removed servo.py and works in lower and uppercase

--- slack_bot.py
+++ slack_bot.py
@@ -6,36 +6,38 @@ from slackclient import SlackClient
 # Insert you bots token
 BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 # What channel your bot uses
-CHANNEL_NAME = "general"
+CHANNEL_NAME = "sales-bell"
 
 def servo():
-	GPIO.setmode(GPIO.BOARD)
+    # Sets up GPIO
+    GPIO.setmode(GPIO.BOARD)
 
-	GPIO.setup(7,GPIO.OUT)
+    GPIO.setup(7,GPIO.OUT)
 
-	p = GPIO.PWM(7,50)
-	p.start(7.5)
+    p = GPIO.PWM(7,50)
+    p.start(7.5)
 
-	p.ChangeDutyCycle(10)
-        time.sleep(0.25)
+    # Rings bell 3 times
+    for i in range(3):
+        p.ChangeDutyCycle(10)
+	time.sleep(0.25)
 
-        p.ChangeDutyCycle(3)
-        time.sleep(1)
+	p.ChangeDutyCycle(3)
+	time.sleep(.25)
 	
-        p.ChangeDutyCycle(6)
-        time.sleep(0.5)
+    p.ChangeDutyCycle(6)
+    time.sleep(0.5)
 	
-	p.stop()
+    p.stop()
+    GPIO.cleanup()
 
-	GPIO.cleanup()
 def main():
-    print "main"
     # Creates a slackclient instance with bots token
     sc = SlackClient(BOT_TOKEN)
     
     #Connect to slack
     if sc.rtm_connect():
-        print "connect" 
+        print "connected" 
         while True:
             # Read latest messages
             for slack_message in sc.rtm_read():
@@ -44,7 +46,7 @@ def main():
                 if not message or not user:
                     continue
                 # If someone types ring, the bot will post a bell
-                if(message == "ring"):
+                if(message.lower() == "ring"):
                     sc.rtm_send_message(CHANNEL_NAME, ":bell:")
                     servo() 
             # Sleeps for one second
